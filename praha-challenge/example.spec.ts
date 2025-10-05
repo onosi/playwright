359bcaa9df46ae6b2bdbd2a07f3a5fc911fb9272
onosi <onosi.fumiya@gmail.com>
2025-10-05 19:31:09 +0900

    feat: テストを実装

--- praha-challenge/example.spec.ts
+++ praha-challenge/example.spec.ts
@@ -28,9 +28,16 @@ test('Hotel Planisphereの会員登録画面で会員登録できる', async ({ 
   await page.getByLabel('一般会員').check();
   await page.getByLabel('住所').fill('東京都新宿区');
   await page.getByLabel('電話番号').fill('01133335555');
-  await page.getByLabel('性別').selectOption({ index: 0 });
+  await page.getByLabel('性別').selectOption({ index: 1 });
   await page.getByLabel('生年月日').fill('1990-01-01');
   await page.getByRole('button', { name: '登録' }).click();
 
   await expect(page).toHaveURL(/mypage.html/);
+  await expect(page.locator('body')).toContainText('yamada@example.com');
+  await expect(page.locator('body')).toContainText('山田太郎');
+  await expect(page.locator('body')).toContainText('一般会員');
+  await expect(page.locator('body')).toContainText('東京都新宿区');
+  await expect(page.locator('body')).toContainText('01133335555');
+  await expect(page.locator('body')).toContainText('男性');
+  await expect(page.locator('body')).toContainText('1990年1月1日');
 });
