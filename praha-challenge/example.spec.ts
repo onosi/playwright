bd22db95f958a1a65b2769ac5571105b2bd0f80c
onosi <onosi.fumiya@gmail.com>
2025-10-05 19:25:13 +0900

    feat:会員登録処理の実装

--- praha-challenge/example.spec.ts
+++ praha-challenge/example.spec.ts
@@ -16,3 +16,21 @@ test('Hotel Planisphereの会員登録画面にアクセスできる', async ({ 
 
   await expect(page).toHaveURL(/signup.html/);
 });
+
+test('Hotel Planisphereの会員登録画面で会員登録できる', async ({ page }) => {
+  await page.goto('https://hotel.testplanisphere.dev/ja/signup.html');
+
+  await page.getByLabel('メールアドレス').fill('yamada@example.com');
+  await page.getByLabel('パスワード 必須').fill('password123');
+  await page.getByLabel('パスワード（確認） 必須').fill('password123');
+  await page.getByRole('button', { name: '登録' }).click();
+  await page.getByLabel('氏名').fill('山田太郎');
+  await page.getByLabel('一般会員').check();
+  await page.getByLabel('住所').fill('東京都新宿区');
+  await page.getByLabel('電話番号').fill('01133335555');
+  await page.getByLabel('性別').selectOption({ index: 0 });
+  await page.getByLabel('生年月日').fill('1990-01-01');
+  await page.getByRole('button', { name: '登録' }).click();
+
+  await expect(page).toHaveURL(/mypage.html/);
+});
