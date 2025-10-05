8eb776527a7c67c5322dc78f7abc17bbf31dd01d
onosi <onosi.fumiya@gmail.com>
2025-10-05 19:07:03 +0900

    feat: 課題3実装

--- praha-challenge/example.spec.ts
+++ praha-challenge/example.spec.ts
@@ -6,3 +6,13 @@ test('Hotel Planisphereのサイトにアクセスできる', async ({ page }) =
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/HOTEL PLANISPHERE/);
 });
+
+test('Hotel Planisphereの会員登録画面にアクセスできる', async ({ page }) => {
+  await page.goto('https://hotel.testplanisphere.dev/ja/');
+  const link = page.getByRole('link', { name: '会員登録' })
+  expect(link).toBeVisible();
+  await link.click();
+  await page.waitForURL('**/signup.html');
+
+  await expect(page).toHaveURL(/signup.html/);
+});
